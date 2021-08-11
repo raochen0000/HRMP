6dfffbaf615b733fcd1b8f22cfcb2ac30a95530d
Rogers <raochen960822@gmail.com>
2021-08-11 15:52:19 +0800

    主页样式完善

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -2,28 +2,26 @@
   <div class="navbar">
     <hamburger :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" />
 
-    <breadcrumb class="breadcrumb-container" />
+    <div class="app-breadcrumb">
+      江苏传智播客教育科技股份有限公司
+      <span class="breadBtn">体验版</span>
+    </div>
 
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img src="@/assets/common/bigUserHeader.png" class="user-avatar">
+          <span class="user-name">超级管理员</span>
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
             <el-dropdown-item>
-              Home
+              首页
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-            <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a>
           <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
+            <span style="display:block;">退出</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
@@ -33,12 +31,10 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
 
 export default {
   components: {
-    Breadcrumb,
     Hamburger
   },
   computed: {
@@ -64,7 +60,7 @@ export default {
   height: 50px;
   overflow: hidden;
   position: relative;
-  background: #fff;
+  background-image: -webkit-linear-gradient(left, #3d6df8, #5b8cff);
   box-shadow: 0 1px 4px rgba(0,21,41,.08);
 
   .hamburger-container {
@@ -80,9 +76,24 @@ export default {
     }
   }
 
-  .breadcrumb-container {
-    float: left;
+.app-breadcrumb {
+  display: inline-block;
+  font-size: 18px;
+  line-height: 50px;
+  margin-left: 10px;
+  color: #ffffff;
+  cursor: text;
+  .breadBtn {
+    background: #84a9fe;
+    font-size: 14px;
+    padding: 0 10px;
+    display: inline-block;
+    height: 30px;
+    line-height: 30px;
+    border-radius: 10px;
+    margin-left: 15px;
   }
+}
 
   .right-menu {
     float: right;
@@ -115,22 +126,26 @@ export default {
       margin-right: 30px;
 
       .avatar-wrapper {
-        margin-top: 5px;
         position: relative;
-
+        display: flex;
+        align-items: center;
+        cursor: pointer;
         .user-avatar {
-          cursor: pointer;
           width: 40px;
           height: 40px;
           border-radius: 10px;
         }
-
+        .user-name {
+          margin: 0 10px;
+          font-size: 18px;
+          color: #fff;
+        }
         .el-icon-caret-bottom {
-          cursor: pointer;
           position: absolute;
           right: -20px;
-          top: 25px;
-          font-size: 12px;
+          top: 15px;
+          font-size: 18px;
+          color: #fff;
         }
       }
     }
--- src/permission.js
+++ src/permission.js
@@ -1,10 +1,13 @@
 import router from './router'
 import store from './store'
 // import { Message } from 'element-ui'
-// import NProgress from 'nprogress' // progress bar
-// import 'nprogress/nprogress.css' // progress bar style
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
+
+const whiteList = ['/login', '/404'] // 定义路由白名单
 
 router.beforeEach((to, from, next) => {
+  NProgress.start() // 开启进度条
   // 判断有无token
   if (store.getters.token) {
     // 有token，是否去登录页
@@ -13,5 +16,17 @@ router.beforeEach((to, from, next) => {
     } else {
       next()
     }
+  } else {
+    // 没有token，是否在白名单中
+    if (whiteList.indexOf(to.path) === -1) {
+      next('/login')
+    } else {
+      next()
+    }
   }
+  NProgress.done() // 强制关闭进度条
+})
+
+router.afterEach(() => {
+  NProgress.done() // 路由跳转成功关闭进度条
 })
